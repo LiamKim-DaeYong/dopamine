709b0031a520fc0c548e03bd79e3da5ed3369000
Liamkim <liamkim1018@gmail.com>
2025-06-06 18:39:50 +0900

    refactor(i18n): migrate to @AutoConfiguration for Spring Boot 3.x compatibility

--- modules/i18n/dopamine-i18n/src/main/kotlin/io/dopamine/i18n/config/I18nAutoConfiguration.kt
+++ modules/i18n/dopamine-i18n/src/main/kotlin/io/dopamine/i18n/config/I18nAutoConfiguration.kt
@@ -2,15 +2,15 @@ package io.dopamine.i18n.config
 
 import io.dopamine.i18n.resolver.MessageResolver
 import io.dopamine.i18n.resolver.SpringMessageResolver
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import java.util.Locale
 
-@Configuration
+@AutoConfiguration
 @EnableConfigurationProperties(I18nProperties::class)
 class I18nAutoConfiguration {
     @Bean
